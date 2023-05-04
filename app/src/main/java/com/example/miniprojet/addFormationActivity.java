4a51f822497d550f08d7a5a3f846a1dc437ba45c
ali-ben-maouia <alibenmaouia111@gmail.com>
2023-05-04 19:49:16 +0100

    formation

---
+++ app/src/main/java/com/example/miniprojet/addFormationActivity.java
@@ -0,0 +1,25 @@
+package com.example.miniprojet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class addFormationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_formation);
+        EditText title;
+        EditText description;
+        EditText dateDebut;
+        EditText dateFin;
+        TextView imgName;
+        Button img;
+        Button save;
+
+    }
+}
