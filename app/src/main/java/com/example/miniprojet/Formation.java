83c907f3a8adeeeade136a95bd41d610c3661ff0
ali-ben-maouia <alibenmaouia111@gmail.com>
2023-05-04 19:47:56 +0100

    formation

---
+++ app/src/main/java/com/example/miniprojet/Formation.java
@@ -0,0 +1,15 @@
+package com.example.miniprojet;
+
+public class Formation {
+    public int id;
+    public String description;
+    public String title;
+    public String img;
+
+    public Formation(int id, String description, String title, String img) {
+        this.id = id;
+        this.description = description;
+        this.title = title;
+        this.img = img;
+    }
+}
